44c08b9cc8db2c17ee50b116a3f0340f06f1ae2e
mallitooo <71378346+mallitooo@users.noreply.github.com>
2021-10-22 21:29:52 +0200

    Same but for this file
    Uploaded the changes we did yesterday into my repository

--- project1-image_filter/filter.py
+++ project1-image_filter/filter.py
@@ -1,47 +1,70 @@
 import multiprocessing as mp
-import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cProfile
+import functions as my
+import numpy as np
+from multiprocessing.sharedctypes import Array
+import ctypes
+# import myfunctions as my
 
-import myfunctions as my
+
 
 # sets the number of cores
-NUMCORES = 4
+NUMCORES = mp.cpu_count()
 
-# constructs a 500x1000 matrix
-matrix_1 = np.random.rand(500,1000)
+# opens images
+image = np.array(Image.open('lena.png'))
 
-# constructs a 1000x500 matrix
-matrix_2 = np.random.rand(1000,500)
+# sets the kernels
+image_filter1 = np.ones((3, 3))
+image_filter2 = np.ones((1, 3))
 
-# initializes global matrix "matrix_2" which is shared memory. Multiple threads can read from this matrix
-my.init_second(matrix_2.shape,matrix_2)
+# initializes image
 
-# assign variables to the shape of matrix_1
-(rows,columns) = matrix_1.shape
+print(f'Image shape: {image.shape}')
 
-# sequential multiplication of matrix_1 and matrix_2
-for f in range(rows):
-    v = matrix_1[f,:]
-    c = my.parallel_matmul(v)
+def tonumpyarray(mp_arr):
+    # mp_array is a shared memory array with lock
 
-# splits matrix_1 into a list of rows
-v = list()
-for f in range(row):
-    v.append(matrix_1[f,:])
+    return np.frombuffer(mp_arr.get_obj(), dtype=np.uint8)
 
 
-# executes the parallel multiplication
-# maps the list v to parallel_matmul which gets executed in a thread pool
-def matrix_multiplication(v):
-    with mp.Pool(processes = NUMCORES,initializer = my.init_second,initargs = [matrix_2.shape,matrix_2]) as p:
-        result = p.map(my.parallel_matmul,v)
-    return result
+#HERE YOU HAVE TO DEFINE THE MULTIPROCESSING VECTOR FOR IMAGE1
+data_buffer1_size=image.shape[0]*image.shape[1]*image.shape[2]
+shared_space1= Array(ctypes.c_byte,data_buffer1_size)
+filtered_image1_VECTOR=tonumpyarray(shared_space1)
+filtered_image1= filtered_image1_VECTOR.reshape(image.shape)
 
-# obtains the multiplication result from the parallel execution
-parallel_result = matrix_multiplication(v)    
+#HERE YOU HAVE TO DEFINE THE MULTIPROCESSING VECTOR FOR IMAGE2
+data_buffer2_size=image.shape[0]*image.shape[1]*image.shape[2]
+shared_space2= Array(ctypes.c_byte,data_buffer2_size)
+filtered_image2_VECTOR=tonumpyarray(shared_space2)
+filtered_image2= filtered_image2_VECTOR.reshape(image.shape)
 
+NUMCORES = mp.cpu_count()
 
+my.image_filter(image,image_filter1,NUMCORES,filtered_image1)
+my.image_filter(image,image_filter2,NUMCORES,filtered_image2)
 
+print('hey')
+# # executes the parallel filtering processes
+# def filters_execution(image,filter1,filter2,numprocessors,filtered_image1,filtered_image2):
+#     # creates a lock to handle memory access
+#     lock = mp.Lock()
+#
+#     # define and start the processes
+#     p1 = mp.Process(target=my.image_filter,args=(image,filter1,numprocessors,filtered_image1))
+#     p2 = mp.Process(target=my.image_filter,args=(image,filter2,numprocessors,filtered_image2))
+#
+#     p1.start()
+#     p2.start()
+#
+#     # wait until the processes have ended
+#     p1.join()
+#     p2.join()
+#
+#
+#
+# filters_execution(image1,filter1,filtered_image2,NUMCORES,filtered_image1,filtered_image2)
