cb719ce4da31f5230d59bfa8a31e202fde5898da
Andy Ortiz <a.ortizg@uniandes.edu.co>
2021-10-25 23:08:44 +0200

    Final with comments.

--- project1-image_filter/filter.py
+++ project1-image_filter/filter.py
@@ -9,28 +9,35 @@ import numpy as np
 from multiprocessing.sharedctypes import Array
 import ctypes
 
-# sets the number of cores
+# sets the number of cores that will be destined to the thread pool
 NUMCORES = int(mp.cpu_count() / 2)
 
-# opens images
+# opens the image to be filtered
 image = np.array(Image.open('lena.png'))
 
-# sets the kernels
+# sets the kernels that will be used to filter the images
+
+# this filter echoes the original image with no changes 
 filter1 = np.array([
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 1, 0, 0],
     [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-])
+    [0, 0, 0, 0, 0]])
+
+# this is a first order edge filter by rows
 filter2 = np.array([0.5, 0, -0.5])
+
+# this is first order filter by columns
 filter3 = np.array([[0.5], [0], [0.5]])
 
+# this is a second-order bi-directional edge filter 
 filter4 = np.array([
     [1, 0, -1],
     [2, 0, -2],
-    [1, 0, -1]
-])
+    [1, 0, -1]])
+
+# this is a gaussian blur filter
 filter5 = np.array([
     [0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633],
     [0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965],
@@ -38,8 +45,12 @@ filter5 = np.array([
     [0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965],
     [0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633]
 ])
-filter6=np.array([0.5, 0 ,0,0, -0.5])
-filter7=np.array([[0.5],[0],[0],[0],[0.5]])
+
+# this is a test 1x5 filter
+filter6 = np.array([0.5, 0, 0, 0, -0.5])
+
+# this is a test 5x1 filter
+filter7 = np.array([[0.5], [0], [0], [0], [0.5]])
 
 # initializes image
 print(f'Processing image..')
@@ -51,26 +62,37 @@ def tonumpyarray(mp_arr):
     return np.frombuffer(mp_arr.get_obj(), dtype=np.uint8)
 
 
-# HERE YOU HAVE TO DEFINE THE MULTIPROCESSING VECTOR FOR IMAGE1
+# defines the mp vector1 for the image
 data_buffer1_size = image.shape[0] * image.shape[1] * image.shape[2]
 shared_space1 = Array(ctypes.c_byte, data_buffer1_size)
 
-# HERE YOU HAVE TO DEFINE THE MULTIPROCESSING VECTOR FOR IMAGE2
+# defines the mp vector2 for the image
 data_buffer2_size = image.shape[0] * image.shape[1] * image.shape[2]
 shared_space2 = Array(ctypes.c_byte, data_buffer2_size)
 
+
 def filters_execution(p_image, p_filter1, p_filter2, p_numprocessors, p_shared_space1,
                       p_shared_space2):
+    """
+    This functions executes the two processes that will use the thread pools to run the fulters
+
+    :param p_image: the image to be filtered
+    :param p_filter1: the first filter
+    :param p_filter2: the seconds filter
+    :param p_numprocessors: the number of processors to be used
+    :param p_shared_space1: the first shared memory space
+    :param p_shared_space2: the second shared memory space
+    """
     # creates a lock to handle memory access
     lock = mp.Lock()
 
     # define and start the processes
     p1 = mp.Process(target=my.image_filter, args=(p_image, p_filter1, p_numprocessors,
                                                   p_shared_space1))
-
     p2 = mp.Process(target=my.image_filter, args=(p_image, p_filter2, p_numprocessors,
                                                   p_shared_space2))
 
+    # start the processes
     p1.start()
     p2.start()
 
@@ -78,8 +100,7 @@ def filters_execution(p_image, p_filter1, p_filter2, p_numprocessors, p_shared_s
     p1.join()
     p2.join()
 
-
-
+# define this in order to run the filters
 if __name__ == '__main__':
     filters_execution(image, filter4, filter5, NUMCORES, shared_space1, shared_space2)
 
@@ -105,7 +126,3 @@ if __name__ == '__main__':
     plt.title("Filter2")
 
     plt.show()
-
-
-
-
--- project1-image_filter/functions.py
+++ project1-image_filter/functions.py
@@ -6,22 +6,76 @@ import ctypes
 import numpy as np
 
 
+# Auxiliary functions
+def tonumpyarray(mp_arr):
+    """
+    This function creates a numpy array of uint8
+    :param mp_arr: the array to be transformed
+    """
+    # mp_array is a shared memory array with a lock
+    return np.frombuffer(mp_arr.get_obj(), dtype=np.uint8)
+
+
+def pool_init(shared_array_, srcimg, imgfilter):
+    """
+    This initializes the global shared memory data
+
+    :param shared_array_: the shared array containaing the data that has a lock and must be in
+    vector format
+    :param srcimg: the source image to be used
+    :param imgfilter: the image filter that will be applied
+    """
+    # defines the local memory reference for the shared memory space
+    global shared_space
+    # defines the numpy matrix handler
+    global shared_matrix
+
+    # defines the read only data
+    global image
+    global my_filter
+
+    # here, we initialize the global read only memory data
+    image = srcimg
+    my_filter = imgfilter
+    size = image.shape
+
+    # assigns the shared memory  to the local reference
+    shared_space = shared_array_
+
+    # defines the numpy matrix reference to handle data, which will use the shared memory buffer
+    shared_matrix = tonumpyarray(shared_space).reshape(size)
+
+
+def dp(x, y):
+    """
+    This function simplifies the process of calculating the dot product
+    :param x: the first element of the dot product
+    :param y: the second element of the dot product
+    """
+
+    r = 0
+
+    for i in range(len(x)):
+        r += float(x[i] * y[i])
+
+    return r
+
+
+# Filter functions
 def image_filter(p_image, p_filter, p_numprocessors, p_shared_space):
-    '''
+    """
+    This image filter receives the image to be filtered and the filter that will be applied
+    and classifies the filter based on the dimmension
 
-    :param p_image:
-    :param p_filter:
-    :param p_numprocessors:
-    :param p_shared_space:
-    :return:
+    :param p_image:  the image to be filtered
+    :param p_filter: the image kernel
+    :param p_numprocessors: the number of processors to use in the algorithm
+    :param p_shared_space: the shared space of the destination image
 
-    '''
+    """
+
     # size contains the dimmensions of the filter
     size = p_filter.shape
-    # Until here we had it OK yesterday
-
-    # Now we will make define an object function that contains the filter that will be used in
-    # the pool function according to the dim of the filter: func.chosenfilter
 
     rows = range(p_image.shape[0])
 
@@ -39,14 +93,14 @@ def image_filter(p_image, p_filter, p_numprocessors, p_shared_space):
             p.map(squared_filter, rows)
 
 
-'''
-Here I'm fusing the two variants of the colums filters into one named columned_filter to pass it to the object function when corresponding 
-'''
-
-
 def columned_filter(r):
+    """
+    This function applies a filter defined by columns and determines if the filter is
+    3x1 or 5x1.
+    :param r: the row that will be operated on
+    """
     # Global memory recalling 
-    global image
+    global image, res_row
     global my_filter
     global shared_space
 
@@ -56,8 +110,12 @@ def columned_filter(r):
     # obtains the current row of the image
     c_row = image[r, :, :]
 
-    # edge cases
-    if (my_filter.shape[0] == 5):
+    # handles the case for when the filter is 5x1
+    if my_filter.shape[0] == 5:
+
+        # the below section is the logic that is required to handle the edge cases for when the
+        # previous and previous-previous rows are undefined.
+
         # sets the previous row to the current row if we are in the first row or the row index is
         # negative
         p_row = image[r - 1, :, :] if r > 0 else image[r, :, :]
@@ -75,6 +133,9 @@ def columned_filter(r):
         # sets the next row to the current row if we are in the last row 
         n_row = image[r, :, :] if r == (rows - 1) else image[r + 1, :, :]
 
+        # the below section is the logic that is required to handle the edge cases for when the
+        # next and next-next rows are undefined.
+
         # sets the next row to the current row if we are in the last row
         if r <= (rows - 3):
             nn_row = image[r + 2, :, :]
@@ -89,18 +150,21 @@ def columned_filter(r):
         # for each layer in the image
         for d in range(depth):
 
-            # for each pixel in the row
+            # for each pixel in the row, apply the convolution
             for i in range(cols - 1):
                 res_row[i, d] = int((pp_row[i, d] * my_filter[4] + p_row[i, d] * my_filter[3] +
                                      c_row[i, d] * my_filter[2] + n_row[i, d] * my_filter[1] +
                                      nn_row[i, d] * my_filter[0]))
-
             # res_row contains the filtered row that will be saved into the shared space
-    elif (my_filter.shape[0] == 3):
 
-        # edge cases
+    # handles the case for when the filter is 3x1
+    elif my_filter.shape[0] == 3:
 
-        # sets the previous row to the current row if we are in the first row or the row index is negative
+        # the below section is the logic that is required to handle the edge cases for when the
+        # previous row is undefined or if the next row is invalid for this memory spaces
+
+        # edge cases sets the previous row to the current row if we are in the first row or the
+        # row index is negative
         p_row = image[r - 1, :, :] if r > 0 else image[r, :, :]
 
         # sets the next row to the current row if we are in the last row
@@ -121,76 +185,21 @@ def columned_filter(r):
 
         # res_row contains the resulting array as before 
 
-    # Once out of the if loop, when we have computed in one of the ways res_row, we will copy it to the shared space 
-    # And now we will unlock the shared memory in order to rewrite the row in its place: 
+    # Once out of the if loop, when we have computed in one of the ways res_row, we will copy it
+    # to the shared space And now we will unlock the shared memory in order to rewrite the row in
+    # its place:
     with shared_space.get_lock():
-        # while we are in this code block no ones, except this execution thread, can write in the shared memory
+        # while we are in this code block only the current execution thread can write to the
+        # shared memory
         shared_matrix[r, :, :] = res_row
-
-    # ----------------------------------------------------------------------------------------------------
-
-
-# This functions just create a numpy array structure of type unsigned int8, with the memory used
-# by our global r/w shared memory
-def tonumpyarray(mp_arr):
-    # mp_array is a shared memory array with lock
-
-    return np.frombuffer(mp_arr.get_obj(), dtype=np.uint8)
-
-# This function initialize the global shared memory data
-
-def pool_init(shared_array_, srcimg, imgfilter):
-    # shared_array_: is the shared read/write data, with lock. It is a vector (because the shared
-    # memory should be allocated as a vector srcimg: is the original image imgfilter is the
-    # filter which will be applied to the image and stor the results in the shared memory array
-
-    # We defines the local process memory reference for shared memory space
-    global shared_space
-    # Here we define the numpy matrix handler
-    global shared_matrix
-
-    # Here, we will define the readonly memory data as global (the scope of this global variables
-    # is the local module)
-    global image
-    global my_filter
-
-    # here, we initialize the global read only memory data
-    image = srcimg
-    my_filter = imgfilter
-    size = image.shape
-
-    # Assign the shared memory  to the local reference
-    shared_space = shared_array_
-    # Defines the numpy matrix reference to handle data, which will uses the shared memory buffer
-    shared_matrix = tonumpyarray(shared_space).reshape(size)
-
-
-'''
-Here I'm going to include my functions and changes in order to have them organised
-'''
-
-
-def dp(x, y):
-    '''
-    We will create this function (dot product)to ease the process of applying the filter:
-    '''
-    # The inputs are just two vectors of the same length and dot multiplies them
-    r = 0
-
-    for i in range(len(x)):
-        r += float(x[i] * y[i])
-
-    return r
-
-
-'''
-Here we will define the first approach for the combined functions squared filter and row filters 
-'''
 
 
 def squared_filter(row):
-    ''''''
-    # Global memory recalling 
+    """
+    This function applies a square filter and determiens if its 3x3 or 5x5
+    :param row: the row that will be operated on
+    """
+    # Global memory recalling
     global image
     global my_filter
     global shared_space
@@ -201,26 +210,25 @@ def squared_filter(row):
     # Creation of the rows that must be taken into account for a 3x3:
     srow = image[row, :, :]
 
-    if (row > 0):
+    if row > 0:
         prow = image[row - 1, :, :]
     else:
         prow = image[row, :, :]
 
-    if (row == (rows - 1)):
+    if row == (rows - 1):
         nrow = srow
     else:
         nrow = image[row + 1, :, :]
 
-    # Now we will define the extra rows in the case the filter is a 5x5 
-
+    # Now we will define the extra rows in the case the filter is a 5x5
     if size == 5:
 
-        if (row > 1):
+        if row > 1:
             p2row = image[row - 2, :, :]
         else:
             p2row = prow
 
-        if (row >= (rows - 2)):
+        if row >= (rows - 2):
             n2row = nrow
         else:
             n2row = image[row + 2, :, :]
@@ -256,70 +264,60 @@ def squared_filter(row):
                                  dp(n2row[i - 2:i + 3, j], my_filter[4, :])
                                  )
 
-        # And now the boundary positions: 
-
+        # handle edge cases on the boundary
         frow[0, :] = frow[2, :]
         frow[1, :] = frow[2, :]
         frow[cols - 2, :] = frow[cols - 3, :]
         frow[cols - 1, :] = frow[cols - 3, :]
 
-    ''' Need to be checked if the names are correct for this last part, the one with the global memories '''
-
-    # And now we will unlock the shared memory in order to rewrite the row in its place: 
+    # unlock the shared memory in order to rewrite the row in its place
     with shared_space.get_lock():
-        # while we are in this code block no ones, except this execution thread, can write in the shared memory
         shared_matrix[row, :, :] = frow
 
     return
 
 
-# ------------------------------------------------------------------------------------------------
-
 def rowed_filter(row):
+    """
+    This function applies a filter defined by rows and determines if the filter is
+    1x3 or 1x5.
+    :param row: the row that will be operated on
+     """
+
     # Global memory recalling
     global image
     global my_filter
     global shared_space
 
     (rows, cols, depth) = image.shape
-    size = len(my_filter)  # !!! It shall be a single 1x5 vector
+    size = len(my_filter)
 
-    # Initialization of the result vector and the row we are filtering:  
-
+    # initialize the rows used
     srow = image[row, :, :]
     frow = np.zeros((cols, depth))
 
-    # Application of the filter: 
-
-    # First we will fulfill the general case:
+    # handles the case when the filter is 1x5
     if size == 5:
         for j in range(depth):
             for i in range(2, cols - 2):
                 frow[i, j] = int(dp(srow[i - 2:i + 3, j], my_filter))
-
-                # And now we will fulfill the two borders with the most-adjacent value
 
         frow[0, :] = frow[2, :]
         frow[1, :] = frow[2, :]
         frow[cols - 2, :] = frow[cols - 3, :]
         frow[cols - 1, :] = frow[cols - 3, :]
 
+    # handles the case when the filter ix 1x3
     else:
         for j in range(depth):
             for i in range(1, cols - 1):
                 frow[i, j] = int(dp(srow[i - 1:i + 2, j], my_filter))
-            # And now we will fulfill the two borders with the most-adjacent value
 
         frow[0, :] = frow[1, :]
         frow[cols - 1, :] = frow[cols - 2, :]
 
-    '''Need to be checked if the names are correct for this last part, the one with the global 
-    memories '''
-
-    # And now we will unlock the shared memory in order to rewrite the row in its place: 
+    # unlock the shared memory in order to rewrite the row in its place
     with shared_space.get_lock():
-        # while we are in this code block no ones, except this execution thread, can write in the
-        # shared memory
         shared_matrix[row, :, :] = frow
 
     return
